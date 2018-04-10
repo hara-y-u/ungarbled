1db58359195d8c601a403e344a80301b8d975d3a
yukihiro hara <yukihr@gmail.com>
2018-04-10 18:58:06 +0900

    🐛

--- lib/ungarbled/action_controller_ext.rb
+++ lib/ungarbled/action_controller_ext.rb
@@ -6,36 +6,7 @@ module Ungarbled
   module ActionControllerExt
     extend ActiveSupport::Concern
 
-    included do
-      helper_method :encode_filename
-      helper_method :encode_filename_for_zip_item
-      alias_method :send_file_headers_with_encode_filename!, :send_file_headers!
-      alias_method_chain :send_file_headers!, :encode_filename
-    end
-
-    def self.prepended(mod)
-      mod.helper_method :encode_filename
-      mod.helper_method :encode_filename_for_zip_item
-    end
-
-    private
-
-    def filename_encoder(options)
-      @filename_encoder ||= Ungarbled::Encoder.new(browser)
-      lang = options[:lang] ||
-                 Rails.configuration.ungarbled.default_lang
-      @filename_encoder.tap { |e| e.lang = lang }
-    end
-
-    def encode_filename(filename, options = {})
-      filename_encoder(options).encode(filename)
-    end
-
-    def encode_filename_for_zip_item(filename, options = {})
-      filename_encoder(options).encode_for_zip_item(filename)
-    end
-
-    def send_file_headers!(options)
+    SEND_FILE_HEADERS_PROC = lambda do |options|
       if !::Rails.configuration.ungarbled.disable_auto_encode &&
          options[:filename]
         options[:filename] = encode_filename(options[:filename])
@@ -46,6 +17,38 @@ module Ungarbled
       else
         super
       end
+    end
+
+    included do
+      helper_method :encode_filename
+      helper_method :encode_filename_for_zip_item
+      define_method :send_file_headers_with_encode_filename!, &SEND_FILE_HEADERS_PROC
+      alias_method_chain :send_file_headers!, :encode_filename
+    end
+
+    def self.prepended(mod)
+      class << mod
+        define_method :send_file_headers!, &SEND_FILE_HEADERS_PROC
+      end
+      mod.helper_method :encode_filename
+      mod.helper_method :encode_filename_for_zip_item
+    end
+
+    private
+
+    def filename_encoder(options)
+      @filename_encoder ||= Ungarbled::Encoder.new(browser)
+      lang = options[:lang] ||
+             Rails.configuration.ungarbled.default_lang
+      @filename_encoder.tap { |e| e.lang = lang }
+    end
+
+    def encode_filename(filename, options = {})
+      filename_encoder(options).encode(filename)
+    end
+
+    def encode_filename_for_zip_item(filename, options = {})
+      filename_encoder(options).encode_for_zip_item(filename)
     end
   end
 end
